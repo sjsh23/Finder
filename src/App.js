c44a0c1693e02b6c301694e810b0ac773ea08b2a
Leeyungsen <frederickmaxwellee02@gmail.com>
2024-05-02 20:23:41 +0700

    test

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+          Edit <code>src/App.js</code> and save to reload. 
+        </p> 
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -21,5 +21,7 @@ function App() {
     </div>
   );
 }
+
+// Josh kita buat backendnya pake apa?
 
 export default App;
