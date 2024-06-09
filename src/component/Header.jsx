4afe20d2c64639256f120521320642a1aaada7ea
sjsh23 <satyasiddhijosh23@gmail.com>
2024-06-09 22:35:27 +0700

    link button

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -2,14 +2,14 @@ const Header = () => {
     return(
         <header className="App-header">
             <nav className="navbar">
-            <div className="navbar-brand">FINDER</div>
+            <a href="/"><div className="navbar-brand">FINDER</div></a>
             <div className="navbar-search">
                 <input type="text" placeholder="Masukkan nama lokasi/area/alamat" />
                 <button>Cari</button>
             </div>
             <div className="navbar-menu">
                 <a href="#">Cari Apa?</a>
-                <button>Masuk</button>
+                <a href="/RegisterPage"><button>Masuk</button></a>
             </div>
             </nav>
         </header>
